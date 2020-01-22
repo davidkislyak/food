3a17c3c8a80b0e7e70368e53f910aa7983a5cbbd
David Kislyak <dkislyak@mail.greenriver.edu>
2020-01-22 14:08:06 -0500

    Add f3 examples, and template for order page

--- index.php
+++ index.php
@@ -22,10 +22,55 @@
     });
 
     //.htaccess required for routing
-    //Define a breakfast root
+    //Define a breakfast route
     $f3->route('GET /breakfast', function() {
         $view = new Template();
         echo $view->render('views/breakfast.html');
+    });
+
+    //Define a breakfast/buffet route
+    $f3->route('GET /breakfast/buffet', function() {
+        $view = new Template();
+        echo $view->render('views/breakfast-buffet.html');
+    });
+
+    //Define a lunch route
+    $f3->route('GET /lunch', function() {
+        $view = new Template();
+        echo $view->render('views/lunch.html');
+    });
+
+    $f3->route('GET /order', function() {
+        $view = new Template();
+        echo $view->render('views/form1.html');
+    });
+
+    //Define a route that accepts a food parameter
+    $f3->route('GET /@item', function($f3, $params) {
+//        var_dump($params);
+        $item  = $params['item'];
+
+        $foodsWeServe = array("tacos", "pizza", "lumpia");
+        if (!in_array($item, $foodsWeServe)) {
+            echo "<p>Sorry, we don't serve $item</p>";
+        } else {
+
+            echo "<p>You ordered $item</p>";
+        }
+
+        switch($item) {
+            case 'tacos':
+                echo "<p>We serve tacos on Tuesday</p>";
+                break;
+            case 'pizza':
+                echo "<p>Pepperoni or veggie?</p>";
+                break;
+            case 'bagels':
+                $f3->reroute("/breakfast");
+                break;
+            default:
+                $f3->error(404);
+        }
     });
 
     //Run fat free
